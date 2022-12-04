d1ed6b837b1bce2980a602e13eb91b738b60bed5
Salman <salmansamiikhan@gmail.com>
2022-12-05 04:03:44 +0600

    05-Add-images-and-render-products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -6,7 +7,22 @@ function App() {
         <a href="/">amazona</a>
       </header>
       <main>
-        list products
+      <h1>Featured Products</h1>
+        <div className="products">
+        {
+          data.products.map(product => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <div className="product-info">
+              <a href={`product/${product.slug}`}>
+              <p>{product.name}</p>
+              </a>
+              <p><strong>{product.price}</strong></p>
+              <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
