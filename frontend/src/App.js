e355b14fb024e49cf55becbabadf66f74859a26a
Salman <salmansamiikhan@gmail.com>
2022-12-05 19:01:05 +0600

    06.Add-Route-and-Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,22 @@
 import data from "./data";
-
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">amazona</a>
-      </header>
-      <main>
-      <h1>Featured Products</h1>
-        <div className="products">
-        {
-          data.products.map(product => (
-            <div className="product" key={product.slug}>
-              <img src={product.image} alt={product.name} />
-              <div className="product-info">
-              <a href={`product/${product.slug}`}>
-              <p>{product.name}</p>
-              </a>
-              <p><strong>{product.price}</strong></p>
-              <button>Add to Cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">amazonia</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
