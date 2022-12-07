99e3dab8383ab58e35d81ee82d34a8d7082b6145
Salman <salmansamiikhan@gmail.com>
2022-12-07 22:21:06 +0600

    08-Fetch-Products

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ import express from 'express'
 import data from './data.js'
 const app = express()
 
-app.get('/api/product', (req, res) => {
-    res.send(data)
+app.get('/api/products', (req, res) => {
+    res.send(data.products)
 })
 
-const port = process.env.PORT || 500
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`)
